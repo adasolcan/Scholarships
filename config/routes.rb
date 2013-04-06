4895643353304c58686bad198c1253a985e96d3e
Andreea Anghelache <andreeaanghelache@student.(none)>
2013-04-06 17:10:46 +0300

    no via post

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Team9Scholarships::Application.routes.draw do
   # omniauth
   match '/auth/:provider/callback', :to => 'user_sessions#create'
   match '/auth/failure', :to => 'user_sessions#failure'
-  match '/user_new_session', :to => 'user_sessions#create', :via => [:post]
+  match '/user_new_session', :to => 'user_sessions#create'
   match '/autentificare', :to => redirect("#{PROVIDER_URL}/auth/autentificare/authorize") 
 
   # Custom logout
