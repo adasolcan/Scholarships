8f3ca2fc4e2493df73e203ff52fee38a4c3bb7de
adasolcan <adasolcan@yahoo.com>
2013-04-08 11:43:13 +0300

    changed root to applications#index

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,16 @@ Team9Scholarships::Application.routes.draw do
 
   resources :applications
   
-  root :to => 'static_pages#index'
+  root :to => 'applications#index'
   # omniauth
   match '/auth/:provider/callback', :to => 'user_sessions#create'
   match '/auth/failure', :to => 'user_sessions#failure'
 
   # Custom logout
   match '/logout', :to => 'user_sessions#destroy'
-  
+
+
+
   #students by specialization
    
 end
