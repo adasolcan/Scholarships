e54b2fe9f3dbdaf46ae5524f65b84b15b15529bc
adasolcan <adasolcan@yahoo.com>
2013-04-08 11:26:47 +0300

    modificare app id si app secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
-APP_ID = '123456789'
-APP_SECRET = 'secret'
+APP_ID = '5d6d6b67-db3c-4315-abe6-f482f241ba5f'
+APP_SECRET = '6ce42bcbffa315f14d9a4ba82f752d17fcad4850f5f336edbb'
 
 CUSTOM_PROVIDER_URL = 'http://fmi-autentificare.herokuapp.com'
 
